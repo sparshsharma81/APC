50ec9d2e562003766f11a2b05aeafc0a0f568354
sparshsharma81 <81arvindarora@gmail.com>
2025-07-14 14:40:11 +0530

    Lect10

---
+++ Lecture10(synchronisation)(14 july)/Test.java
@@ -0,0 +1,17 @@
+public class Test{
+    static int count =0;
+    public static void main(String args[]){
+        Thread1 t1 = new Thread1();
+        Thread2 t2 = new Thread2();
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        //join method can give interrupted exception which is check excecption ---which would result in breaking of code
+        System.out.println(count);
+    }
+    public static void count(){
+        count++;
+    }
+}
